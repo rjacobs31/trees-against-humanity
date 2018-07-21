81d314cfc01b19cbcbae0ccf069c93d764968d1c
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-07-21 19:50:36 +0200

    Add blank card stub.

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, div, h1, program, text)
 import Html.Attributes exposing (class)
-import Widgets exposing (answerCard, questionCard)
+import Widgets exposing (answerCard, blankCard, questionCard)
 
 
 -- MODEL
@@ -35,6 +35,8 @@ view model =
         [ h1 [ class "main-title" ] [ text "Trees Against Humanity" ]
         , questionCard "This is some card, huh?"
         , answerCard "And this is another awesome card."
+        , blankCard Nothing
+        , blankCard <| Just "This is a witty answer submitted by the user."
         , div [ class "hand-container" ]
             [ answerCard "And this."
             , answerCard "Is my."
--- src/Widgets.elm
+++ src/Widgets.elm
@@ -1,7 +1,8 @@
-module Widgets exposing (questionCard, answerCard)
+module Widgets exposing (answerCard, blankCard, questionCard)
 
 import Html exposing (Attribute, Html, div, text)
 import Html.Attributes exposing (class)
+import Maybe exposing (withDefault)
 
 
 questionCard : String -> Html msg
@@ -12,3 +13,8 @@ questionCard message =
 answerCard : String -> Html msg
 answerCard message =
     div [ class "card card--answer" ] [ text message ]
+
+
+blankCard : Maybe String -> Html msg
+blankCard message =
+    div [ class "card card--answer" ] [ text <| withDefault "_____" message ]
