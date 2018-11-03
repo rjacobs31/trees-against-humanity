ca624b6b96acd1ea38d712ab5e16612eef5558c2
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2018-11-03 17:06:49 +0200

    Add file watch mode to Gulp.

--- Gulpfile.js/index.js
+++ Gulpfile.js/index.js
@@ -1,12 +1,39 @@
-const { dest, parallel, series, src } = require('gulp')
+const { dest, parallel, series, src, watch } = require('gulp')
 
 const gulpClean = require('gulp-clean')
 const elm = require('gulp-elm')
 const rename = require('gulp-rename')
 
+function defaultExec() {
+    return series(clean(), build())
+}
+
 function clean() {
-    return src(['dist/**/*.css', 'dist/**/*.html', 'dist/**/*.js'])
+    return parallel(cleanElm, cleanHtml, cleanScripts, cleanStyles)
+}
+
+function cleanElm() {
+    return src(['dist/js/elm.js'])
         .pipe(gulpClean())
+}
+
+function cleanHtml() {
+    return src(['dist/**/*.html'])
+        .pipe(gulpClean())
+}
+
+function cleanScripts() {
+    return src(['dist/**/*.js', '!dist/js/elm.js'])
+        .pipe(gulpClean())
+}
+
+function cleanStyles() {
+    return src(['dist/**/*.css'])
+        .pipe(gulpClean())
+}
+
+function build() {
+    return parallel(html, scripts, styles, transpile)
 }
 
 function html() {
@@ -31,6 +58,24 @@ function transpile() {
         .pipe(dest('dist/js/'))
 }
 
-exports.clean = clean
-exports.build = parallel(html, scripts, styles, transpile)
-exports.default = series(exports.clean, exports.build)
+function watchAll() {
+    return watch(
+        [
+            'client/**/*.html',
+            'client/**/*.js',
+            'client/**/*.css',
+            'client/**/*.elm'
+        ],
+        series(clean(), build())
+    )
+}
+
+exports.clean = clean()
+exports.build = build()
+exports.html = html
+exports.scripts = scripts
+exports.styles = styles
+exports.transpile = transpile
+exports.watch = watchAll
+
+exports.default = defaultExec()
